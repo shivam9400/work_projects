c45df2d72fa1ce945f2a1c5e7d3c389e511e7d27
Shivam Sharma <120392545+shivam9400@users.noreply.github.com>
2025-07-21 14:23:08 +0530

    Update streamlit_app.py

--- AeroDocSense/streamlit_app.py
+++ AeroDocSense/streamlit_app.py
@@ -1,29 +1,22 @@
 import streamlit as st
-st.write("App Started!")
-
-
 from app.generate import answer_query
 import streamlit as st
 from configs import settings
 
-
 HF_TOKEN = settings.HF_TOKEN
 MONGO_URI = settings.MONGO_DB_URI
 
-st.write("HF_TOKEN loaded?", HF_TOKEN is not None)
-st.write("Mongo URI:", MONGO_URI)
+st.set_page_config(page_title="AeroDocSense", layout="centered")
 
-# st.set_page_config(page_title="AeroDocSense", layout="centered")
+st.title("AeroDocSense - RAG for Aircraft hydraulic systems")
+st.markdown("Ask a question based on ATA 29")
 
-# st.title("AeroDocSense - RAG for Aircraft hydraulic systems")
-# st.markdown("Ask a question based on ATA 29")
+query = st.text_input("Enter your query", placeholder="e.g., What are maintenance procedures for A320 engines?")
 
-# query = st.text_input("Enter your query", placeholder="e.g., What are maintenance procedures for A320 engines?")
-
-# if st.button("Generate Answer"):
-#     if query.strip():
-#         with st.spinner("Generating answer..."):
-#             response = answer_query(query)
-#             st.success(response)
-#     else:
-#         st.warning("Please enter a query.")
+if st.button("Generate Answer"):
+    if query.strip():
+        with st.spinner("Generating answer..."):
+            response = answer_query(query)
+            st.success(response)
+    else:
+        st.warning("Please enter a query.")
